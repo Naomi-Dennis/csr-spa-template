47dd4fc66f290a6ede8445096d7b6a2bafca3ea1
Naomi-Dennis <naomi@8thlight.com>
2025-05-13 06:50:26 -0400

    Add cors

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,11 +7,12 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import { onCall } from "firebase-functions/v2/https";
+import {onCall} from "firebase-functions/v2/https";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = onCall({}, (event) => {
-    return { message: "Hello World!" }
-})
+export const helloWorld = onCall({cors:
+  [/web-page-sandbox\.firebaseapp\.com$/]}, (event) => {
+  return {message: "Hello World!"};
+});
